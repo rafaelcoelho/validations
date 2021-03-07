ddffbacd0cfb7c6b9f84211466e5618e8f01dd6f
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2021-03-07 10:23:58 -0300

    Initial commit
    - Set the project structure

---
+++ src/main/java/com/persoal/validation/DomainModelValidationApplication.java
@@ -0,0 +1,13 @@
+package com.persoal.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainModelValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainModelValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/persoal/validation/DomainModelValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persoal.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainModelValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
